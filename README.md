5e29d7f4ea8765c0318b422cae7ee023f574871d
Ai Nguyen <ainguyenkaka@gmail.com>
2018-05-03 10:02:03 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# StatNLP Crawling ACL Author Demo
+
+## Development
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
